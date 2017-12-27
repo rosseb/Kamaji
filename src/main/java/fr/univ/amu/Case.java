bf73c60cea5593044521715d3dc898c60fe7775f
Quentin Rathonie <quentin.rathonie@gmail.com>
2017-12-27 22:12:17 +0100

    J’ai avancé sur le projet j’ai fait ça : Refait les deux classes existantes, GrilleJeu et Case Refait les méthodes existantes et surtout dans GrilleJeu ajouté une méthode qui créé une grille (trouvée sur internet, provisoire en attendant qu’on sache comment générer une grille avec au moins une solution), et une méthode qui raye les cases qu’on lui passe en paramètre
    J’ai mis des commentaires assez explicites dans le code essayez de faire pareil c’est pratique pour voir comment ça marche sans avoir besoin de regarder le code en détail

--- src/main/java/fr/univ/amu/Case.java
+++ src/main/java/fr/univ/amu/Case.java
@@ -3,42 +3,47 @@ package fr.univ.amu;
 public class Case {
 
     private final int valeur;
-    private boolean utilise;
-    private int nbUtilisation;
+    private int raye;
 
     public Case(int valeur) {
         this.valeur = valeur;
-        this.utilise = false;
-        this.nbUtilisation=0;
+        this.raye = 0;
     }
 
     public int getValeur() {
         return valeur;
     }
 
-    public boolean estUtilise() {
-        return utilise;
-    }
-
-    public void utilisationCase() {
-        if(this.nbUtilisation==0 && this.valeur==1){
-            ++this.nbUtilisation;
-        } else {
-            this.utilise = true;
+    public boolean estUtilisee() {
+        if (this.valeur == 1) {
+            if (this.raye >= 2)
+                return true;
+            else
+                return false;
+        }
+        else {
+            if (this.raye >= 1)
+                return true;
+            else
+                return false;
         }
     }
+
+    public void rayer() {
+        this.raye++;
+    }
+
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Case)) return false;
         Case caseo = (Case) o;
         return this.valeur==(caseo.getValeur()) &&
-                this.utilise==caseo.estUtilise();
+                this.estUtilisee()==caseo.estUtilisee();
     }
 
     public void reinitialiser(){
-        this.nbUtilisation=0;
-        this.utilise=false;
+        this.raye=0;
     }
 
 }
--- src/main/java/fr/univ/amu/GrilleJeu.java
+++ src/main/java/fr/univ/amu/GrilleJeu.java
@@ -1,12 +1,82 @@
 package fr.univ.amu;
-
+import java.util.ArrayList;
 
 public class GrilleJeu
 {
-    private Case[][] matrice ;
+    private Case[][] matrice;
+    private final int valeurSomme;
 
-    public GrilleJeu(Case[][] matrice) {
-        this.matrice = matrice;
+    public GrilleJeu(int valeurSomme, int largeurGrille) {
+        this.valeurSomme = valeurSomme;
+        this.matrice = this.constituerGrilleAleatoire(largeurGrille);
+    }
+
+    private Case[][] constituerGrilleAleatoire(int largeurGrille) {
+        // Constituer une grille résolvable
+
+
+        // PROVISOIRE : Grille trouvée sur internet, il faudra remplacer par un algo qui créé une grille pour laquelle il existe au moins une solution
+
+        // Créer tous les objets cases (la grille : http://prntscr.com/ht2bjl)
+        Case c1 = new Case(3);
+        Case c2 = new Case(3);
+        Case c3 = new Case(2);
+        Case c4 = new Case(2);
+        Case c5 = new Case(4);
+        Case c6 = new Case(3);
+        Case c7 = new Case(2);
+        Case c8 = new Case(1);
+        Case c9 = new Case(1);
+        Case c10 = new Case(3);
+        Case c11 = new Case(3);
+        Case c12 = new Case(2);
+        Case c13 = new Case(5);
+        Case c14 = new Case(2);
+        Case c15 = new Case(4);
+        Case c16 = new Case(3);
+        Case c17 = new Case(2);
+        Case c18 = new Case(2);
+        Case c19 = new Case(4);
+        Case c20 = new Case(2);
+        Case c21 = new Case(2);
+        Case c22 = new Case(4);
+        Case c23 = new Case(1);
+        Case c24 = new Case(3);
+        Case c25 = new Case(3);
+
+
+        Case[][] aRetourner =   {   {c1, c2, c3, c4, c5},
+                                    {c6, c7, c8, c9, c10},
+                                    {c11, c12, c13, c14, c15},
+                                    {c16, c17, c18, c19, c20},
+                                    {c21, c22, c23, c24, c25}    };
+
+        return aRetourner;
+    }
+
+    public boolean rayerCases(ArrayList<Case> lesCasesArayer) {
+        // Retourner true si les cases ont bien été reliées entre elles
+        // Retourner false si impossible de les reliers (une des cases déjà utilisée)
+
+        int valeurTotale = 0;
+        for (Case c : lesCasesArayer) {
+            valeurTotale++;
+            if (c.estUtilisee())
+                return false;
+        }
+
+        // Si aucune des cases n'est déjà utilisée (rappel : la fonction estUtilisee() gère seule le fait que 1 peut être rayé 2 fois)
+
+        // On vérifie que la somme des cases est égale à la valeur à trouver (this.valeurSomme)
+        if (valeurTotale != this.valeurSomme)
+            return false;
+
+        // Tout est bon on peut relier les cases ensembles
+        for (Case c : lesCasesArayer) {
+            c.rayer();
+        }
+
+        return true;
     }
 
     public void reinitialiserGrille(){
