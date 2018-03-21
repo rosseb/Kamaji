46d3ab960af84260326f0c1cb1a1644dcdd43f2f
mehdialami2018 <34450334+mehdialami2018@users.noreply.github.com>
2018-03-21 19:27:21 +0100

    Update ListeGrilles.java

--- src/main/java/fr/univ/amu/Mahdi/ListeGrilles.java
+++ src/main/java/fr/univ/amu/Mahdi/ListeGrilles.java
@@ -1,156 +1,216 @@
 package fr.univ.amu.Mahdi;
 
+
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JButton;
 
-public class ListeGrilles {
-
-	public ArrayList<JButton> grille7;
-	public ArrayList<JButton> grille8;
+public class LesGrilles {
+	
+	ArrayList<JButton> Grille5;
+	ArrayList<JButton> Grille7;
+	ArrayList<JButton> Grille8;
 
 	
-	public ListeGrilles() {
+public LesGrilles() {
 	
-		ArrayList<JButton> grille7= new ArrayList<JButton>();
-		ArrayList<JButton> grille8= new ArrayList<JButton>();
+	Grille5= new ArrayList<JButton>();
+	
+	JButton case1 = new JButton("3");	 
+	JButton case2 = new JButton("3");
+	JButton case3 = new JButton("2");
+	JButton case4 = new JButton("2");
+	JButton case5 = new JButton("4");
+	JButton case6 = new JButton("3"); 
+	JButton case7 = new JButton("2");
+	JButton case8 = new JButton("1");
+	JButton case9 = new JButton("1");
+	JButton case10 = new JButton("3");
+	JButton case11 = new JButton("3");
+	JButton case12 = new JButton("2");
+	JButton case13 = new JButton("5");
+	JButton case14 = new JButton("2");
+	JButton case15 = new JButton("4");
+	JButton case16 = new JButton("3");
+	JButton case17 = new JButton("2");
+	JButton case18 = new JButton("2");
+	JButton case19 = new JButton("4");
+	JButton case20 = new JButton("2");
+	JButton case21 = new JButton("2");
+	JButton case22 = new JButton("4");
+	JButton case23 = new JButton("1");
+	JButton case24 = new JButton("3");	
+	JButton case25 = new JButton("3");
+	
+	Collections.addAll(Grille5,case1,case2,case3,case4,case5,case6,case7,case8,case9,case10,case11,case12,case13,case14,case15,case16,case17,case18,case19,case20,case21,case22,case23,case24,case25);
+	
+	Grille7= new ArrayList<JButton>();
+	
+	JButton b1 = new JButton("4");	 
+	JButton b2 = new JButton("2");
+	JButton b3 = new JButton("2");
+	JButton b4 = new JButton("5");
+	JButton b5 = new JButton("2");
+	JButton b6 = new JButton("4"); 
+	JButton b7 = new JButton("3");
+	
+	JButton b8 = new JButton("3");
+	JButton b9 = new JButton("2");
+	JButton b10 = new JButton("5");
+	JButton b11 = new JButton("2");
+	JButton b12 = new JButton("5");
+	JButton b13 = new JButton("2");
+	JButton b14 = new JButton("5");
+	
+	JButton b15 = new JButton("2");
+	JButton b16 = new JButton("2");
+	JButton b17 = new JButton("5");
+	JButton b18 = new JButton("3");
+	JButton b19 = new JButton("3");
+	JButton b20 = new JButton("6");
+	JButton b21 = new JButton("1");
+	
+	JButton b22 = new JButton("2");
+	JButton b23 = new JButton("5");
+	JButton b24 = new JButton("3");	
+	JButton b25 = new JButton("7");
+	JButton b26 = new JButton("4");
+	JButton b27 = new JButton("2");
+	JButton b28 = new JButton("3");
+	
+	JButton b29 = new JButton("3");
+	JButton b30 = new JButton("2");
+	JButton b31 = new JButton("2");
+	JButton b32 = new JButton("2");
+	JButton b33 = new JButton("4");
+	JButton b34 = new JButton("2");
+	JButton b35 = new JButton("5");
+	
+	JButton b36 = new JButton("3");
+	JButton b37 = new JButton("3");
+	JButton b38 = new JButton("1");
+	JButton b39 = new JButton("5");
+	JButton b40 = new JButton("2");
+	JButton b41 = new JButton("3");
+	JButton b42 = new JButton("2");
+	
+	JButton b43 = new JButton("2");
+	JButton b44 = new JButton("2");
+	JButton b45 = new JButton("3");
+	JButton b46 = new JButton("6");
+	JButton b47 = new JButton("2");
+	JButton b48 = new JButton("3");
+	JButton b49 = new JButton("2");
+
+	Collections.addAll(Grille7,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21,b22,b23,b24,b25,b26,b27,b28,b29,b30,b31,b32,b33,b34,b35,b36,b37,b38,b39,b40,b41,b42,b43,b44,b45,b46,b47,b48,b49);
 
 	
-// Grille 7/7
-		
-		grille7.add(new JButton("4"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("4"));
-		grille7.add(new JButton("3"));
-		
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("6"));
-		grille7.add(new JButton("1"));
-		
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("7"));
-		grille7.add(new JButton("4"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("3"));
-		
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("4"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("5"));
-		
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("1"));
-		grille7.add(new JButton("5"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("2"));
-		
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("6"));
-		grille7.add(new JButton("2"));
-		grille7.add(new JButton("3"));
-		grille7.add(new JButton("2"));
-
-		
-// GRILLE 8/8
-		
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("3"));
-		
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("7"));
-		grille8.add(new JButton("4"));
-		
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("1"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("4"));
-		
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("1"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("2"));
-		
 	
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("7"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("6"));
-		grille8.add(new JButton("3"));
-		
-		grille8.add(new JButton("8"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("6"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("2"));
-		
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("6"));
-		grille8.add(new JButton("4"));
-		grille8.add(new JButton("2"));
-		grille8.add(new JButton("5"));
-		grille8.add(new JButton("3"));
-		grille8.add(new JButton("6"));
+	Grille8= new ArrayList<JButton>();
 
+	JButton z1 =new JButton("2");
+	JButton z2 =new JButton("5");
+	JButton z3 =new JButton("3");
+	JButton z4 =new JButton("4");
+	JButton z5 =new JButton("4");
+	JButton z6 =new JButton("3");
+	JButton z7 =new JButton("2");
+	JButton z8 =new JButton("3");
+	
+	JButton x1 =new JButton("3");
+	JButton x2 =new JButton("2");
+	JButton x8 =new JButton("4");
+	JButton x3 =new JButton("2");
+	JButton x4 =new JButton("4");
+	JButton x5 =new JButton("3");
+	JButton x6 =new JButton("7");
+	JButton x7 =new JButton("4");
+	
+	JButton c1 =new JButton("3");
+	JButton c2 =new JButton("4");
+	JButton c3 =new JButton("5");
+	JButton c8 =new JButton("4");
+	JButton c4 =new JButton("4");
+	JButton c5 =new JButton("1");
+	JButton c6=new JButton("3");
+	JButton c7 =new JButton("4");
+	
+	JButton v1 =new JButton("4");
+	JButton v2 =new JButton("5");
+	JButton v3 =new JButton("2");
+	JButton v4 =new JButton("3");
+	JButton v5 =new JButton("1");
+	JButton v6 =new JButton("5");
+	JButton v7 =new JButton("2");
+	JButton v8 =new JButton("2");
+	
+
+	JButton d1 =new JButton("3");
+	JButton d2 =new JButton("2");
+	JButton d3 =new JButton("5");
+	JButton d4 =new JButton("4");
+	JButton d5 =new JButton("7");
+	JButton d6 =new JButton("3");
+	JButton d7 =new JButton("6");
+	JButton d8 =new JButton("3");
+	
+	JButton e1 =new JButton("8");
+	JButton e2 =new JButton("2");
+	JButton e3 =new JButton("2");
+	JButton e4 =new JButton("3");
+	JButton e5 =new JButton("2");
+	JButton e6 =new JButton("5");
+	JButton e7 =new JButton("2");
+	JButton e8 =new JButton("5");
+	
+	JButton i1 =new JButton("3");
+	JButton i2 =new JButton("6");
+	JButton i3 =new JButton("4");
+	JButton i4 =new JButton("4");
+	JButton i5 =new JButton("3");
+	JButton i6 =new JButton("2");
+	JButton i7 =new JButton("5");
+	JButton i8 =new JButton("2");
+
+	JButton l1 =new JButton("5");
+	JButton l2 =new JButton("2");
+	JButton l3 =new JButton("6");
+	JButton l4 =new JButton("4");
+	JButton l5 =new JButton("2");
+	JButton l6 =new JButton("5");
+	JButton l7 =new JButton("3");
+	JButton l8 =new JButton("6");
+	
+	
+	
+	
+Collections.addAll(Grille8,z1,z2,z3,z4,z5,z6,z7,z8,x1,x2,x3,x4,x5,x6,x7,x8,c1,c2,c3,c4,c5,c6,c7,c8,v1,v2,v3,v4,v5,v6,v7,v8,d1,d2,d3,d4,d5,d6,d7,d8,e1,e2,e3,e4,e5,e6,e7,e8,i1,i2,i3,i4,i5,i6,i7,i8,l1,l2,l3,l4,l5,l6,l7,l8);
+
+	
+	
+	
+}
+
+public ArrayList<JButton> getGrille5() { 
+	return Grille5;
 	}
+
+public ArrayList<JButton> getGrille7() { 
+	return Grille7;
+	}
+
+
+public ArrayList<JButton> getGrille8() { 
+	return Grille8;
+	}
+
+	
+	
+	
+	
+	
 	
 	
 	
